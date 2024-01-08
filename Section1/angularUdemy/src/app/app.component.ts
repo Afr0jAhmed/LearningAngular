b7d3c9b3f47d82865bceb0746ac81893ff97213c
afrobug <afroj@whydonate.com>
2024-01-08 19:28:54 +0530

    forms module

--- Section1/angularUdemy/src/app/app.component.ts
+++ Section1/angularUdemy/src/app/app.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FormBuilder],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  name = 'Afroj';
+  title = 'angularUdemy';
 }
