6cb64bc3458ad2570772c26467e4bd2eb244ffa3
afrobug <afroj@whydonate.com>
2024-02-13 19:00:15 +0530

    Component selector

--- Section2/new-component/src/app/app.component.ts
+++ Section2/new-component/src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  // styleUrls: ['./app.component.css']
+  styles: [`
+  h3{
+    color: red;  
+  }`
+  ]  
+  //inline style is taking over
 })
 export class AppComponent {
-  title = 'new-component';
+  // title = 'new-component';
 }
--- Section2/new-component/src/app/servers/servers.component.ts
+++ Section2/new-component/src/app/servers/servers.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `<app-server></app-server>`,
+  // selector: '.app-servers',
+  template: `<app-server></app-server>
+  <app-server></app-server>`,
   styleUrl: './servers.component.css'
 })
 export class ServersComponent {
