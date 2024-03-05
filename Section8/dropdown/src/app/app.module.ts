ce599dd70280e879e77ee2d77a176290996e0fde
afrobug <afrojahmed6@gmail.com>
2024-03-05 16:16:20 +0530

    functional dropdown

---
+++ Section8/dropdown/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DropdownDirective } from './shared/dropdown.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DropdownDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
